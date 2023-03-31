0df0d61b24bf78e2b0b665f81fdbc87960f31eb8
David Espino <davidespinog@gmail.com>
2023-03-31 12:52:17 -0600

    Change port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const routerApi = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 
@@ -9,7 +9,7 @@ app.use(express.json());
 routerApi(app);
 
 app.use(errorHandler);
-
+console.log(port)
 app.listen(port, () => {
   console.log('Corriendo desde el puerto: ', port)
 })
