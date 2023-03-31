f6191999e670aa7430fe2d8c9b15cb335606ea61
David Espino <davidespinog@gmail.com>
2023-03-31 15:51:11 -0600

    Add enviroment variable

--- db/db.js
+++ db/db.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise')();
-const url = 'postgres://david:nSrfH7GcfGHPz3mRU4PaiWWVmwe4h9Lj@dpg-cgjj5se4dadak46b6nm0-a/daviddata'
+const url = process.env.DATABASE_URL;
 const db = pgp(url);
 
 
